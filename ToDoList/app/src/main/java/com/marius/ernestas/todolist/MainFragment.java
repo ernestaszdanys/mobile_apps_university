fab8888d1d554dc365cb7d6b0c94322abb2458f0
ernestaszdanys <ernestas13@gmail.com>
2014-10-09 09:38:40 +0300

    first commit

---
+++ ToDoList/app/src/main/java/com/marius/ernestas/todolist/MainFragment.java
@@ -0,0 +1,36 @@
+package com.marius.ernestas.todolist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Ernestas on 2014.10.09.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        final TextView text = (TextView) rootView.findViewById(R.id.textView);
+        final Button button = (Button) rootView.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("balta siena");
+            }
+        });
+
+        return rootView;
+    }
+}
