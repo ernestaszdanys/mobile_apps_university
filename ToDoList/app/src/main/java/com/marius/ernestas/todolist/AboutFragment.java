36464a4c89bb206efee5bc8c56faba7f160d5df7
Marius Žilinskas <zilinskas.m@gmail.com>
2014-10-19 01:26:06 +0300

    Fixed about fragment, added functionality and button drawable xml's.

--- ToDoList/app/src/main/java/com/marius/ernestas/todolist/AboutFragment.java
+++ ToDoList/app/src/main/java/com/marius/ernestas/todolist/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.marius.ernestas.todolist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,18 +10,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * Created by Ernestas on 2014.10.15.
- */
 public class AboutFragment extends Fragment {
+
+    private Button contactButton;
+    private Button likeButton;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
-        Button contactButton = (Button) rootView.findViewById(R.id.contactButton);
-        Button likeButton = (Button) rootView.findViewById(R.id.likeButton);
+        contactButton = (Button) rootView.findViewById(R.id.buttonContact);
+        likeButton = (Button) rootView.findViewById(R.id.buttonLike);
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleContactButton();
+            }
+        });
+
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleLikeButton();
+            }
+        });
 
         return rootView;
+    }
+
+    //TODO: test intents
+    private void handleContactButton() {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("plain/text");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ernestas13@gmail.com", "zilinskas.m@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "I would like to contact you about our application!");
+        startActivity(Intent.createChooser(emailIntent, ""));
+    }
+
+    private void handleLikeButton() {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+        browserIntent.setData(Uri.parse("http://facebook.com/neegzistuoja"));
+        startActivity(browserIntent);
     }
 }
--- ToDoList/app/src/main/java/com/marius/ernestas/todolist/MainFragment.java
+++ ToDoList/app/src/main/java/com/marius/ernestas/todolist/MainFragment.java
@@ -9,9 +9,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * Created by Ernestas on 2014.10.09.
- */
 public class MainFragment extends Fragment {
 
     public MainFragment() {
@@ -20,16 +17,6 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-        final TextView text = (TextView) rootView.findViewById(R.id.textAbout);
-        final Button button = (Button) rootView.findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                text.setText("balta siena");
-            }
-        });
 
         return rootView;
     }
